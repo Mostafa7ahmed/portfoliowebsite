68708a9ef68c2dfec9c2d41b270c8e94784da664
mostafa hamed <Mostafa7ahmed>
2025-08-14 15:22:02 +0300

    feat: Enhance page components with activeScroll class and implement certificates and contact sections

--- src/app/pages/resume/resume.ts
+++ src/app/pages/resume/resume.ts
@@ -8,6 +8,28 @@ import { Component } from '@angular/core';
   styleUrl: './resume.scss'
 })
 export class Resume {
+   experiences = [
+    {
+      title: 'Altromeda, Front End Developer (Remote)',
+      date: 'June 2025 – Present',
+      description: 'Working on Angular projects focused on performance and scalability. Implementing clean UI, managing state, and collaborating with cross-functional teams to deliver high-quality features.'
+    },
+    {
+      title: 'A Plus, Front End Developer (Remote)',
+      date: 'April 2025 – Present',
+      description: 'Building and maintaining Angular-based web applications. Responsible for creating dynamic UI components, handling API integrations, and ensuring responsive design across devices.'
+    },
+    {
+      title: 'Sintac Code, Front End Developer',
+      date: 'June 2023 – April 2024',
+      description: 'Worked on real projects using React to build interactive web interfaces. Gained hands-on experience in UI design and improved user experience by developing and updating components.'
+    },
+    {
+      title: 'Error Company, Front End Developer (Intern)',
+      date: 'February 2023 – July 2023',
+      description: 'Built and styled web pages using HTML, CSS, and JavaScript. Helped improve user interfaces and fix layout issues.'
+    }
+  ];
   skills = [
     { name: 'HTML & HTML5', value: 90 },
     { name: 'CSS & CSS3', value: 90 },
